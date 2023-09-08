3bbf8cbdf15e25aaf0150fc572c0688de0039dd7
Cesar Herrera <cesarherr321@gmail.com>
2023-09-08 14:09:41 -0300

    Add tests to fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,5 @@
 class Solver
   # Code here
+  def fizzbuzz(n)
+  end
 end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../solver'
+
+describe Solver do
+  describe '#fizzbuzz' do
+    context 'When providing an integer number, check if it is divisible by both 3 and 5' do
+      # Arrange
+      let(:solver) { Solver.new }
+
+      it 'returns "fizz" when divisible by 3' do
+        # Act
+        divisible3 = 3
+
+        # Assert
+        expect(solver.fizzbuzz(divisible3)).to eq('fizz')
+      end
+
+      it 'returns "buzz" when divisible by 5' do
+        # Act
+        divisible5 = 5
+
+        # Assert
+        expect(solver.fizzbuzz(divisible5)).to eq('buzz')
+      end
+
+      it 'returns "fizzbuzz" when divisible by both 3 and 5' do
+        # Act
+        divisible3_and5 = 15
+
+        # Assert
+        expect(solver.fizzbuzz(divisible3_and5)).to eq('fizzbuzz')
+      end
+    end
+  end
+end
