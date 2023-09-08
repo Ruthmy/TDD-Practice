6013aca27841a2d34263dae564181fd40d2d17a3
Cesar Herrera <cesarherr321@gmail.com>
2023-09-08 14:12:03 -0300

    Add fizzbuzz method to the Solver class

--- solver.rb
+++ solver.rb
@@ -1,5 +1,10 @@
 class Solver
   # Code here
   def fizzbuzz(n)
+    return 'fizzbuzz' if (n % 3).zero? && (n % 5).zero?
+    return 'fizz' if (n % 3).zero?
+    return 'buzz' if (n % 5).zero?
+
+    n.to_s
   end
 end
